a84c73390ba749fe4590129a73ac611b9c8f023c
Dbarne <danielbarnett28@gmail.com>
2023-05-17 16:42:36 +0100

    Adding js for form function

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
+});
+
+$(document).ready(function () {
+  $('form').submit(function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the form data
+    var name = $('#name-input').val();
+    var email = $('#email-input').val();
+    var message = $('#message-area').val();
+
+    // Create an object to hold the form data
+    var formData = {
+      name: name,
+      email: email,
+      message: message
+    };
+
+    // Send the AJAX request
+    $.ajax({
+      url: 'https://mail.google.com', // Replace with the actual endpoint URL
+      type: 'POST',
+      data: formData,
+      success: function (response) {
+        // Handle the success response
+        $('#success-message').text('Form submitted successfully!');
+        $('#error-message').text(''); // Clear any previous error message
+      },
+      error: function (xhr, status, error) {
+        // Handle the error response
+        $('#error-message').text('Error sending email: ' + error);
+        $('#success-message').text(''); // Clear any previous success message
+      }
+    });
+  });
+});
