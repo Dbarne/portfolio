4695fdc7e3ac9a452478a196c21e18f4bd1f5fe8
Dbarne <danielbarnett28@gmail.com>
2023-05-17 17:16:57 +0100

    Adding logic for form to reach an endpoint

--- script.js
+++ script.js
@@ -7,37 +7,34 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-$(document).ready(function () {
-  $('form').submit(function (event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    // Get the form data
-    var name = $('#name-input').val();
-    var email = $('#email-input').val();
-    var message = $('#message-area').val();
-
-    // Create an object to hold the form data
-    var formData = {
-      name: name,
-      email: email,
-      message: message
-    };
-
-    // Send the AJAX request
-    $.ajax({
-      url: 'https://mail.google.com', // Replace with the actual endpoint URL
-      type: 'POST',
-      data: formData,
-      success: function (response) {
-        // Handle the success response
-        $('#success-message').text('Form submitted successfully!');
-        $('#error-message').text(''); // Clear any previous error message
-      },
-      error: function (xhr, status, error) {
-        // Handle the error response
-        $('#error-message').text('Error sending email: ' + error);
-        $('#success-message').text(''); // Clear any previous success message
-      }
-    });
+(function() {
+  emailjs.init('_xWo3mpzvZksO-sei');
+  
+  $('form').submit(function(event) {
+      event.preventDefault();
+      
+      var name = $('#name-input').val();
+      var email = $('#email-input').val();
+      var message = $('#message-area').val();
+      
+      var templateParams = {
+          from_name: name,
+          from_email: email,
+          message: message
+      };
+      
+      emailjs.send('service_yc4wkze', 'template_w4cf9q4', templateParams)
+          .then(function(response) {
+              // Email sent successfully
+              $('#success-message').text('Form submitted successfully!');
+              $('#error-message').text('');
+              $('#name-input').val('');
+              $('#email-input').val('');
+              $('#message-area').val('');
+          }, function(error) {
+              // Error sending email
+              $('#error-message').text('Error sending email: ' + error);
+              $('#success-message').text('');
+          });
   });
-});
+})();
